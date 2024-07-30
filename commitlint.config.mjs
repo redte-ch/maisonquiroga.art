e9acf86486282e0e7ab5d0fcf51aabf59238b2d5
Mauko Quiroga <public@mauko.me>
2024-07-31 01:59:46 +0200

    build: add format to pre-commit

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,2 +1,2 @@
 /** @type {import('@commitlint/types').UserConfig} */
-export default {extends: ['@commitlint/config-conventional']};
+export default { extends: ['@commitlint/config-conventional'] }
