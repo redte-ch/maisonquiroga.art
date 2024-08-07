30eeb2eb68a7d8c00b038b803234db10e46ab1f9
Mauko Quiroga <public@mauko.me>
2024-08-07 15:45:56 +0200

    fix: svelte config was being ignored

---
+++ svelte.config.mjs
@@ -0,0 +1,24 @@
+import { vitePreprocess } from '@astrojs/svelte'
+
+/** @type {boolean} */
+const isProd = process.env.NODE_ENV === 'production'
+
+/** @type {string} */
+const prefix = 'component'
+
+/**
+ * Generate a hash.
+ * @param {{ hash: (name: string) => string, name: string }} params
+ * @returns {string}
+ */
+const cssHash = ({ hash, name }) => `${prefix}-${hash(name)}`
+
+/**
+ * @type {object}
+ * @property {object} [compilerOptions]
+ * @property {function} [compilerOptions.cssHash]
+ */
+const options = isProd ? { compilerOptions: { cssHash } } : {}
+
+/** @type {object} */
+export default { ...options, preprocess: vitePreprocess() }
