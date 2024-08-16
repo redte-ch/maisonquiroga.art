ac9db9c4415fe6af5a4c0992ba45d8510992a68e
Mauko Quiroga <public@mauko.me>
2024-08-16 23:55:56 +0200

    refactor: move maybe types from jsdoc to ts

--- src/utils/adt/maybe.ts
+++ src/utils/adt/maybe.ts
@@ -4,45 +4,37 @@
  * @module maybe
  */
 
+/** To narrow down value types. */
 import type { Refinement } from '~/types'
 
-/** @typeParam A */
+/** Data type for values that may or may not be present. */
 export type Maybe<A> = Nothing | Just<A>
 
-/** @typeParam A */
+/** Data type for values that are present. */
 export type Just<A> = Readonly<{
   _tag: 'Just'
   value: A
 }>
 
-/** A type that signifies the absence of a value. */
-export type Nothing = Readonly<{
-  _tag: 'Nothing'
-}>
-
-/** A variable indicating the absence of a value. */
-export const nothing: Maybe<never> = { _tag: 'Nothing' }
-
 /**
- * A function to wrap a value in a {@link Maybe} monad.
+ * Wrap a value in a {@link Maybe} monad.
  *
- * @template A
- * @function
  * @param {A} a - The value being wrapped.
  * @returns {Maybe<A>} - The wrapped value.
  */
 export const just = <A>(a: A): Maybe<A> => ({ _tag: 'Just', value: a })
 
+/** Data type for absent values. */
+export type Nothing = Readonly<{
+  _tag: 'Nothing'
+}>
+
 /**
- * Check if a value is {@link Nothing}.
+ * No value.
  *
- * @function
- * @param {unknown} x - The value to be checked.
- * @returns {boolean} - True if the value is {@link Nothing}, false otherwise.
+ * @see {@link https://www.infoq.com/presentations/Null-References-The-Billion-Dollar-Mistake-Tony-Hoare/}
  */
-export const isNothing: Refinement<Maybe<unknown>, Nothing> = (
-  x: Maybe<unknown>
-): x is Nothing => x && x._tag && x._tag === 'Nothing'
+export const nothing: Maybe<never> = { _tag: 'Nothing' }
 
 /**
  * Check if a value is {@link Just}.
@@ -56,26 +48,36 @@ export const isJust: Refinement<Maybe<unknown>, Just<unknown>> = (
 ): x is Just<unknown> => x && x._tag && x._tag === 'Just'
 
 /**
- * @template A
- * @template B
- * @template C
- * @param {() => B} a - Function to apply when the value is {@link Nothing}.
- * @param {(a: A) => C} b - Function to apply when the value is {@link Just}.
- * @returns {(z: Maybe<A>) => B | C} A function that takes a {@link Maybe} value
- *   and returns the result of applying the appropriate function.
- */
-export type Match = <A, B, C>(
-  a: () => B,
-  b: (a: A) => C
-) => (a: Maybe<A>) => B | C
-
-/**
- * Function to match a {@link Maybe} value.
+ * Check if a value is {@link Nothing}.
  *
  * @function
- * @param {() => B} onNothing - Callback if the value is {@link Nothing}.
- * @param {(a: A) => B} onJust - Callback if the value is {@link Just}.
- * @returns {(x: Maybe<A>) => B} - The matched value.
+ * @param {unknown} x - The value to be checked.
+ * @returns {boolean} - True if the value is {@link Nothing}, false otherwise.
  */
+export const isNothing: Refinement<Maybe<unknown>, Nothing> = (
+  x: Maybe<unknown>
+): x is Nothing => x && x._tag && x._tag === 'Nothing'
+
+/** Callback if a value is {@link Just}. */
+type OnJust<A, B> = (a: A) => B
+
+/** Callback if a value is {@link Nothing}. */
+type OnNothing<A> = () => A
+
+/** Matcher over {@link Maybe} */
+type Matcher<A, B, C> = (a: Maybe<A>) => B | C
+
+/**
+ * @param {OnNothing} a - Function to apply when the value is {@link Nothing}.
+ * @param {OnJust} b - Function to apply when the value is {@link Just}.
+ * @returns {Matcher} A function that takes a {@link Maybe} value and returns the
+ *   result of applying the appropriate callback function.
+ */
+export type Match = <A, B, C>(
+  a: OnNothing<B>,
+  b: OnJust<A, C>
+) => Matcher<A, B, C>
+
+/** Function to match a {@link Maybe} value and apply the appropriate callbacks. */
 export const match: Match = (onNothing, onJust) => (x) =>
   isJust(x) ? onJust(x.value) : onNothing()
